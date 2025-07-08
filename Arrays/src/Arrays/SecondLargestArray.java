e9011586425095b47d52807d8f686bd1fa1a6bd4
burle123 <burleshantanu@gmail.com>
2025-07-08 22:32:42 +0530

    Arrays

---
+++ Arrays/src/Arrays/SecondLargestArray.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class SecondLargestArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int[] arr= {44,55,72,51,18};
+			int large=arr[0];
+			for(int i=1;i<arr.length;i++) {
+				 if(arr[i]>large) {
+					 large=arr[i];
+				}
+				
+			}
+			System.out.println("Largest Num = "+large);
+			 
+	}
+
+}
--- Arrays/src/Arrays/duplicate.java
+++ Arrays/src/Arrays/duplicate.java
@@ -4,14 +4,21 @@ public class duplicate {
 
 	public static void main(String args[]) {
 		
-		int[] arr= {1,2,3,5,6,43,2,11,3,5,8,9,9,0};
+		int[] arr= {1,2,3,5,6,43,2,11,3,5,8,9,9};
 		
 		for(int i=0;i<arr.length;i++) {
 			for(int j=i+1;j<arr.length;j++) {
+				 
 				if(arr[i] == arr[j]) {
-					System.out.println(arr[i]);
+					
+					arr[i]=arr[i]/10;
+					 
 				}
 			}
+		}
+		for (int k:arr) {
+			if(k!=0)
+			System.out.print(k+",");
 		}
 	}
 }
