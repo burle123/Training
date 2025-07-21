ffb054690f98c9c0e3701c7bae731dfe685bc3a6
burle123 <burleshantanu@gmail.com>
2025-07-22 00:47:13 +0530

    ArrayList

---
+++ Arrays/src/Arrays/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package Arrays;
+import java.util.*;
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> list=new ArrayList<>(10);	
+		
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.addFirst(1 ); 	 	
+		
+		System.out.println(list);
+	}
+
+}
